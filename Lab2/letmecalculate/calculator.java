87766d673c50880d8524d4586c4fe87882aa861a
savioratharv <91827941+savioratharv@users.noreply.github.com>
2023-12-11 11:55:14 +0530

    Add files via upload

---
+++ Lab2/letmecalculate/calculator.java
@@ -0,0 +1,16 @@
+package letmecalculate;
+
+public class calculator {
+    public static double add(double x,double y){
+    return x+y;
+    }
+    public static double sub(double x,double y){
+    return x-y;
+    }
+    public static double mul(double x,double y){
+    return x*y;
+    }
+    public static double div(double x,double y){
+    return x/y;
+    }
+}
